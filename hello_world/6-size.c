f5f6b85578ad2fb849f1ceebb987b07f7f4342d0
“DesaraB” <desarabaroti99@gmail.com>
2024-10-21 13:15:04 +0000

    added new file

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,7 +8,7 @@
  {
     printf("Size of a char: %ld byte (s)\n", sizeof(char));
     printf("Size of a int: %ld byte (s)\n", sizeof(int));
-    printf("Size of a a long int: %ld byte (s)\n", sizeof(long int));
+    printf("Size of a a long int: %ld byte (s)\n", sizeof(unsigned int));
     printf("Size of a a long long: %ld byte (s)\n", sizeof(long long));
     printf("Size of a float: %ld byte (s)\n", sizeof(float));
 
