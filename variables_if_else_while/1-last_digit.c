4f9ae985b0c207bbaf1543778a30fb14f22cdd7d
“DesaraB” <desarabaroti99@gmail.com>
2024-10-22 11:44:35 +0000

    added new file

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- * main - Entry point of the program.
- *
- * This function is the entry point for the program.
- *
- * Return: Always 0 (Success).
+ * main - entry point
+ * Return: 0
  */
 int main(void)
 {
@@ -16,14 +12,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	ld = n % 10;
 
 	if (ld > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	else if (ld == 0)
-		printf("Last digit of %d is %d and is 0\n", n, ld);
+		printf("Last digit of %d is %d and is zero\n", n, ld);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
-
+		printf("Last digit of %d is %d and is less than 6 and not zero\n", n, ld);
 	return (0);
+
 }
---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar ('\n');
+}
