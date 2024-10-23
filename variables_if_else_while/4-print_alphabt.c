87bce3b7cb34808a5fb7759e66c3fc76ecfe4b18
“DesaraB” <desarabaroti99@gmail.com>
2024-10-23 08:24:51 +0000

    added new file

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char letter = 'a';
+	while (letter <= 'z')
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
+
+	return(0);
+}
