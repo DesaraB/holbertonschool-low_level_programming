9bc09d8985d5d5d88418f1939cf2f09bcea02447
“DesaraB” <desarabaroti99@gmail.com>
2024-10-24 11:41:01 +0000

    added file

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ *
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
