87f815a9130ef7743afd56b280a29c06ed78abcb
“DesaraB” <desarabaroti99@gmail.com>
2024-10-25 09:42:21 +0000

    added file

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* add - two integers and returns the result
+* @a: the character to check
+* @b: the character to check
+* Return: 0
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
