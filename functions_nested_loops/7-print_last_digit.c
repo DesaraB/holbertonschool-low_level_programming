a413dad39243b2003660b05cefc8e99ac49280b4
“DesaraB” <desarabaroti99@gmail.com>
2024-10-25 09:35:08 +0000

    added file

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
 * print_last_digit - check the code
-* @n: number as input
+* @n :character to check
 * Return: value of last digit
 */
 
@@ -11,12 +11,15 @@ int print_last_digit(int n)
 	int last_digit;
 
 	last_digit = n % 10;
+
 	if (n >= 0)
 	{
+		_putchar('0' + last_digit);
 		return (last_digit);
 	}
 	else
 	{
+		_putchar ('0' - last_digit);
 		return (-last_digit);
 	}
 }
