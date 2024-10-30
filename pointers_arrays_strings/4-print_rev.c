17dc6bfa84324fa63edb767dd3214173d43e4a6b
“DesaraB” <desarabaroti99@gmail.com>
2024-10-30 12:40:42 +0000

    added file that prints a string in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_rev - prints a string in reverse
+* @s: points to the string
+*/
+
+void print_rev(char *s)
+{
+	int i;
+	int last_index;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		last_index = i;
+	}
+	for (i = last_index; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
