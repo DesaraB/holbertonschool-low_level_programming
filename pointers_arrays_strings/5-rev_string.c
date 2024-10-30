67da9c3db6c55bec328cc6070bdbdb125b324445
“DesaraB” <desarabaroti99@gmail.com>
2024-10-30 21:48:09 +0000

    added new file

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,5 +8,31 @@
 
 void rev_string(char *s)
 {
-    
+	int i;
+	int last_index;
+	int length = 0;
+	char tmp;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+		last_index = 1;
+		last_index = length - 1;
+	}
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		
+		last_index = length - 1;
+	}
+
+	if (!last_index)
+		return;
+	for (i = last_index; i >= 0; i--)
+	{
+		tmp = s[i];
+		s[i] = s[last_index];
+		s[last_index--] = tmp;
+	}
+
+
 }
