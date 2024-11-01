6d164717214ecf73f4fd65957e0518d402527d58
“DesaraB” <desarabaroti99@gmail.com>
2024-11-01 09:36:13 +0000

    added file

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+/**
+ * mul -entry point multiply two integers
+ * @a: -holds value for input
+ * @b: -hold value for input
+ * Return: a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
