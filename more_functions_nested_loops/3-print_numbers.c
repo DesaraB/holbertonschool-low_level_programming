aa82c97152eb1da517ccaa86512653402210571d
“DesaraB” <desarabaroti99@gmail.com>
2024-11-01 09:42:34 +0000

    added file

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * print_numbers -entry point.Prints digits
+ */
+void print_numbers(void)
+{
+	int a = 0;
+
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		_putchar(a + '0');
+	}
+
+	_putchar('\n');
+}
