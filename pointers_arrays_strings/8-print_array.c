a1b9a625d907e43552993d6d8cb006d79b0e561b
“DesaraB” <desarabaroti99@gmail.com>
2024-11-01 08:50:08 +0000

    added file that prints elements of an array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_array - rints n elements of an array of integers
+* @a: points to a value
+* @n: points to a value
+*/
+
+void print_array(int *a, int n)
+{
+int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf("\n");
+}
