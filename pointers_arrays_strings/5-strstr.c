57c6436ecfb37a8e8a266ee366fad18a17011663
“DesaraB” <desarabaroti99@gmail.com>
2024-11-05 09:02:11 +0000

    added file that locates a substring

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,26 +1,33 @@
-#include "main.h"
-#include <unistd.h>
+#include"main.h"
 #include <stddef.h>
 
 /**
-* _strstr - locates a substring
-* @haystack: points to a string
-* @needle: points to a string
-*/
+ * _strstr -entry point,locates a substring
+ * @haystack: -holds value for input
+ * @needle: -holds value for input
+ * Return: (NULL)
+ */
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i;
+	int j;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i + j] != needle[j])
+			{
 				break;
+			}
+
+
 		}
 		if (needle[j] == '\0')
-			return (&haystack[i]);
+		{
+			return (haystack + i);
+		}
 	}
 	return (NULL);
 }
