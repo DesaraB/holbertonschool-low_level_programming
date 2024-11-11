b69572f9962bf16349c05153cc95176a38e32668
“DesaraB” <desarabaroti99@gmail.com>
2024-11-11 12:30:06 +0000

    added new file

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - duplicate a string
+ * @str: string to be duplicated.
+ * Return: pointer to the duplicated string, or NULL if insufficient memory
+ * was available.
+ */
+
+char *_strdup(char *str)
+{
+	int size;
+	int i;
+	char *copy;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	size = i;
+
+	copy = malloc(size * sizeof(char));
+	if (copy == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		copy[i] = str[i];
+	}
+
+	return (copy);
+}
