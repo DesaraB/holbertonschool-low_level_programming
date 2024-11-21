6cf4f83a144e9334dd2bdd8d41b1169f64c0bbdb
“DesaraB” <desarabaroti99@gmail.com>
2024-11-21 12:02:20 +0000

    Print all

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,21 +1,7 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
-#include <stdarg.h>
-
-/**
-* struct p_d - structure for printing data
-* @type: format type
-* @print: the function for printing
-*/
-
-typedef struct printer
-{
-	char *type;
-	void (*print)(va_list ag_list);
-} printer_t;
-
-
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
