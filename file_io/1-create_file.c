ac670ef52f73687ce452d0021faf313c27918493
“DesaraB” <desarabaroti99@gmail.com>
2024-12-05 20:25:21 +0000

    creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,18 +12,20 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    int size;
-    int written;
+	int fd;
+	int size;
+	int written;
 
-    if (filename == NULL)
-    {
-        return (-1);
-    }
-
-    fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
-    if (fd < 0)
+	if (filename == NULL)
+	{
 		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	if (fd < 0)
+	{
+		return (-1);
+	}
 
 	if (text_content != NULL)
 	{
@@ -32,8 +34,9 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	if (written == -1)
+	{
 		return (-1);
-
+	}
 	close(fd);
 	return (1);
 }
